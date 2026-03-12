a1178fe1f326ab3f6fb6a20c016784630143d963
Sibashrit Pattnaik <sibashritpattnaik@gmail.com>
2026-03-12 21:22:01 +0530

    bind polyfills

--- javascript-core/bind-call-apply/pollyfills.js
+++ javascript-core/bind-call-apply/pollyfills.js
@@ -28,7 +28,7 @@ greeting.myCall(null, "Berhampur", "Odisha");
 console.log("---------- APPLY POLLYFILL ----------");
 
 Function.prototype.myApply = function (context, args) {
-  const _context = context === null ? globalThis : Object(context);
+  const _context = context == null ? globalThis : Object(context);
   const key = Symbol();
   _context[key] = this;
   const result = _context[key](...args);
@@ -39,3 +39,19 @@ Function.prototype.myApply = function (context, args) {
 greeting.myApply(user1, ["Berhampur", "Odisha"]);
 greeting.myApply(5, ["Berhampur", "Odisha"]);
 greeting.myApply(null, ["Berhampur", "Odisha"]);
+
+console.log("---------- BIND POLLYFILL ----------");
+
+Function.prototype.myBind = function (context, ...bindArgs) {
+  const fn = this;
+  return function (...args) {
+    const _context = context == null ? globalThis : Object(context);
+    const key = Symbol();
+    _context[key] = fn;
+    const result = _context[key](...bindArgs, ...args);
+    delete _context[key];
+    return result;
+  };
+};
+const greetJohn = greeting.myBind(user1, "Berhampur", "India");
+greetJohn();
